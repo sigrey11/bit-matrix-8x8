1ce2b72b88854a5943c355d6b9f43f2822020503
sigrey11 <91280916+sigrey11@users.noreply.github.com>
2021-10-25 14:33:03 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,10 +5,15 @@ DigitalPin.P15,
 DigitalPin.P14,
 DigitalPin.P13
 )
+max7219_matrix.for_4_in_1_modules(
+rotation_direction.clockwise,
+false
+)
 basic.forever(function () {
     max7219_matrix.scrollText(
-    "Hello world!",
+    "бебра",
     0,
     500
     )
+    music.playMelody("F B - F A E C E ", 162)
 })
