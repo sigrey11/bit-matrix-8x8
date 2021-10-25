70a8d001fc452b29fc4c11c6722e5796225d0a84
sigrey11 <91280916+sigrey11@users.noreply.github.com>
2021-10-25 14:18:16 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+max7219_matrix.setup(
+4,
+DigitalPin.P16,
+DigitalPin.P15,
+DigitalPin.P14,
+DigitalPin.P13
+)
+basic.forever(function () {
+    max7219_matrix.scrollText(
+    "Hello world!",
+    0,
+    500
+    )
+})
